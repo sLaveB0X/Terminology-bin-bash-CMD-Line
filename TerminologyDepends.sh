8a8a2051d203402da3aa2f059d90c256d3872386
sLaveB0X <37854667+sLaveB0X@users.noreply.github.com>
2018-03-28 08:04:42 -0400

    Add files via upload

---
+++ TerminologyDepends.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+#This install the dependencies for Terminology.
+
+sudo apt install -y gcc
+sudo apt install -y clang
+sudo apt install -y make
+sudo apt install -y libpam-dev
+sudo apt install -y libpng-dev
+sudo apt install -y libjpeg-dev
+sudo apt install -y zlib1g-dev
+sudo apt install -y libdbus-1-dev
+sudo apt install -y luajit
+sudo apt install -y libx11-dev
+sudo apt install -y libxcursor-dev
+sudo apt install -y libxrender-dev
+sudo apt install -y libxrandr-dev
+sudo apt install -y libxfixes-dev
+sudo apt install -y libxdamage-dev
+sudo apt install -y libxcomposite-dev
+sudo apt install -y libxss-dev
+sudo apt install -y libxp*****
+sudo apt install -y libxext-dev
+sudo apt install -y libxinerama-dev
+sudo apt install -y libxkbfile-dev
+sudo apt install -y libxtst-dev
+sudo apt install -y libxcb
+sudo apt install -y libxcb-shape-dev
+sudo apt install -y libxcb-keysyms1-dev
+sudo apt install -y libpulse-dev
+sudo apt install -y libsndfile-dev
+sudo apt install -y libudev-dev
+sudo apt install -y libblkid-dev
+sudo apt install -y libgstreamer	
+sudo apt install -y libtiff-dev
+sudo apt install -y giflib
+sudo apt install -y libmount-dev
+sudo apt install -y openssl
+sudo apt install -y libspectre-dev
+sudo apt install -y libraw-dev
+sudo apt install -y libxine2-dev
+sudo apt install -y libraw-dev
+sudo apt install -y libxine2-dev:armhf
+sudo apt install -y libbullet-dev
+sudo apt install -y librsvg2-bin
+sudo apt install -y python-poppler
+sudo apt install -y freeglut3-dev
+sudo apt install -y python-opengl
+sudo apt install -y giflib-tools
+sudo apt install -y sudo apt install libgstreamer0.10-0:arm64
+
